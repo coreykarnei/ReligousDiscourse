43445462a18a6196233427cf1ca6ab6f2fb4b20c
coreykarnei <coreykarnei@utexas.edu>
2023-09-29 00:01:11 -0500

    pushing things along, introduced some bugs

--- agents.py
+++ agents.py
@@ -1,10 +1,12 @@
 import openai
 
 def getAgentResponse(api_key, chat_history, agentName, RETRY=5):
+    print("starting get response...")
     # Set the system message
     system_messages = {
-        "Jesus": "[Write your next reply from the point of view of Jesus Christ and only Jesus. Write 1 reply only in internet RP style from the point of view of Jesus Christ having a friendly debate with his friends. Write a measured and thoughtful response that directly addresses the message of the most recent speaker. Write a score on a scale from 1 tp 5 indicating how strongly Jesus Christ wants to be the nest person to reply. Write your reply in the following format:\nJesus: message goes here\n[Desire to respond]: 3",
-        "Buddha": "[Write your next reply from the point of view of The Buddha and only Buddha. Write 1 reply only in internet RP style from the point of view of The Buddha having a friendly debate with his friends. Write a measured and thoughtful response that directly addresses the message of the most recent speaker. Write a score on a scale from 1 tp 5 indicating how strongly the Buddha wants to be the nest person to reply. Write your reply in the following format:\nBuddha: message goes here\n[Desire to respond]: 3"
+        "Jesus": "[Write your next reply from the point of view of Jesus Christ and only Jesus. Write 1 reply only in internet RP style from the point of view of Jesus Christ having a friendly debate with his friends. Write a measured and thoughtful response that directly addresses the message of the most recent speaker. Write a score on a scale from 1 tp 5 indicating how strongly Jesus Christ wants to be allowed to reply next. Write your reply in the following format:\nJesus: message goes here\nDesire to respond: 3",
+        "Buddha": "[Write your next reply from the point of view of The Buddha and only Buddha. Write 1 reply only in internet RP style from the point of view of The Buddha having a friendly debate with his friends. Write a measured and thoughtful response that directly addresses the message of the most recent speaker. Write a score on a scale from 1 tp 5 indicating how strongly the Buddha wants to be allowed to reply next. Write your reply in the following format:\nBuddha: message goes here\nDesire to respond: 3",
+        "Muhammad": "[Write your next reply from the point of view of Muhammad and only Muhammad. Write 1 reply only in internet RP style from the point of view of Muhammad having a friendly debate with his friends. Write a measured and thoughtful response that directly addresses the message of the most recent speaker. Write a score on a scale from 1 tp 5 indicating how strongly Muhammad wants to be allowed to reply next. Write your reply in the following format:\Muhammad: message goes here\nDesire to respond: 3"
     }
 
     # Initialize the OpenAI API client
@@ -22,10 +24,14 @@ def getAgentResponse(api_key, chat_history, agentName, RETRY=5):
 
     # Extract and print the assistant’s reply from the response
     assistant_reply = response['choices'][0]['message']['content']
-    if assistant_reply[0:5] != agentName and isinstance(assistant_reply[-1], int):
+    agetnNameLength= len(agentName)
+    if assistant_reply[0:agetnNameLength] != agentName or not assistant_reply[-1].isnumeric():
         if RETRY > 0:
+            print("query failed; retrying..")
+            print(assistant_reply[0:5], agentName)
+            print(assistant_reply[-1])
             assistant_reply = getAgentResponse(api_key, chat_history, agentName, RETRY-1)
         else:
             return "ERROR IN RESPONSE\n", assistant_reply
 
-    return assistant_reply[:-23], assistant_reply[-1]
+    return assistant_reply[:-21], assistant_reply[-1]
--- interface.py
+++ interface.py
@@ -3,14 +3,34 @@ import copy
 
 # Set your OpenAI GPT-3 API key here
 api_key = "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"
-all_agents = ["Jesus", "Buddha"]
+all_agents = ["Jesus", "Buddha", "Muhammad"]
 
+def getUserInput(remaining_agents):
+    alias = {
+        'b': 'Buddha',
+        'j': 'Jesus',
+        'm': 'Muhammad'
+    }
+    allowed_chars = ['j', 'b', 'm']
+    have_input = False
+    while not have_input:
+        imp = input("Choose who may respond: j, b, or m\n")
+        if imp in allowed_chars:
+            if alias[imp] in remaining_agents:
+                return alias[imp]
+            else:
+                print(f"{alias[imp]} cannot go twice in a row")
+        else:
+            print(f"Invalid input {imp}")
 
-def single_chat_loop(chat_history, current_agent):
+def singleChatLoop(chat_history, previous_agent):
     remaining_agents = copy.deepcopy(all_agents)
-    remaining_agents.remove(current_agent)
+    try:
+        remaining_agents.remove(previous_agent)
+    except:
+        pass
 
-    print("thinking...")
+    print("agents are thinking...")
     replies, scores = [], []
     for agent in remaining_agents:
         reply, score = getAgentResponse(api_key, chat_history, agent)
@@ -23,16 +43,21 @@ def single_chat_loop(chat_history, current_agent):
              print("=", end='')
         print("")
 
-    selected_agent = remaining_agents[0]
-    selected_agent_idx = 0
-    print(selected_agent + ": " + replies[selected_agent_idx])
-    chat_history.append({"role": "user", "content": selected_agent + ": " + replies[selected_agent_idx]})
-    return chat_history
+    selected_agent = getUserInput(remaining_agents)
+    selected_agent_idx = remaining_agents.index(selected_agent)
+    print("selected agent:", selected_agent)
+
+    print(replies[selected_agent_idx])
+    chat_history.append({"role": "user", "content": replies[selected_agent_idx]})
+    return chat_history, selected_agent
+
+debate_topic = "What is the best moral system?"
+print("Debate Topic: " + debate_topic)
 
 chat_history = [
-    {"role": "user", "content": "Debate Topic: What is the meaning of life?"},
-    {"role": "user", "content": "Budda: It is to minimize suffering, as I have taught."}
+    {"role": "user", "content": "Debate Topi: " + debate_topic},
 ]
+chat_history, currentAgent = singleChatLoop(chat_history, "") 
 
-currentAgent = "Buddha"
-chat_history = single_chat_loop(chat_history, currentAgent) 
+while(True):
+    chat_history, currentAgent = singleChatLoop(chat_history, currentAgent) 
